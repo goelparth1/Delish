941ed95ed8ac3e9dcb90af6ce93f56fb451292bf
Parth Goel <parth@ch.iitr.ac.in>
2024-02-08 20:10:14 +0530

    Completed

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">DELISH</div>
+          <div className="right">
+            <p>Civil Lines , Roorkee</p>
+            <p>Open: 9:00 AM - 11:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Parth</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Parth.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
